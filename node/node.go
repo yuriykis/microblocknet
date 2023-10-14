74a6aaa3ab310d6e9263e15feea0bd22382b5dd7
Yuriy Kis <yurakis9@gmail.com>
2023-10-14 20:21:40 +0200

    * feat(node.go): add peersLock and peers map to Node struct * feat(node.go): implement Version method to return the current version of the node * feat(node.go): implement AddPeer method to add a peer to the node's peers map * feat(node.go): implement dialRemote method to establish a connection with a remote node * feat(node.go): implement bootstrapNetwork method to bootstrap the network by connecting to a list of addresses
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- node/node.go
+++ node/node.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"log"
 	"net"
+	"sync"
 
 	"github.com/yuriykis/microblocknet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Node struct {
 	ListenAddress string
 
+	peersLock sync.RWMutex
+	peers     map[proto.NodeClient]*proto.Version
 	proto.UnimplementedNodeServer
 }
 
@@ -50,4 +54,45 @@ func (n *Node) NewTransaction(
 
 func (n *Node) NewBlock(ctx context.Context, b *proto.Block) (*proto.Block, error) {
 	return &proto.Block{}, nil
+}
+
+func (n *Node) Version() *proto.Version {
+	return &proto.Version{
+		Version: "0.0.1",
+	}
+}
+
+func (n *Node) AddPeer(c proto.NodeClient, v *proto.Version) {
+	n.peersLock.Lock()
+	defer n.peersLock.Unlock()
+	n.peers[c] = v
+}
+
+func (n *Node) dialRemote(address string) (*proto.NodeClient, *proto.Version, error) {
+	conn, err := grpc.Dial(
+		address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	client := proto.NewNodeClient(conn)
+	version, err := client.Handshake(context.Background(), n.Version())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &client, version, nil
+}
+
+func (n *Node) bootstrapNetwork(addrs []string) error {
+	for _, addr := range addrs {
+		client, version, err := n.dialRemote(addr)
+		if err != nil {
+			return err
+		}
+		n.AddPeer(*client, version)
+	}
+	return nil
 }
