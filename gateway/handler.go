27bfee917b6687f914194e5348631ef7c16572e2
Yuriy Kis <yurakis9@gmail.com>
2023-11-01 11:03:17 +0100

    * refactor(handler.go): remove unused imports and variables * feat(handler.go): add support for InitTransaction endpoint
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- gateway/handler.go
+++ gateway/handler.go
@@ -5,10 +5,8 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 	"github.com/yuriykis/microblocknet/common/proto"
 	"github.com/yuriykis/microblocknet/common/requests"
-	"github.com/yuriykis/microblocknet/node/secure"
 )
 
 type Handler interface {
@@ -79,7 +77,10 @@ func (h *handler) UTXO(c *gin.Context) {
 }
 
 func (h *handler) InitTransaction(c *gin.Context) {
-	var tx *proto.Transaction
+	var (
+		tx  *proto.Transaction
+		err error
+	)
 	if c.Request.Method == http.MethodPost {
 		var tReq requests.InitTransactionRequest
 		if err := c.ShouldBindJSON(&tReq); err != nil {
@@ -87,54 +88,21 @@ func (h *handler) InitTransaction(c *gin.Context) {
 				"error": err.Error(),
 			})
 		}
-		clientUTXOs, err := h.service.UTXOsByAddress(c.Request.Context(), tReq.FromAddress)
+		tx, err = h.service.InitTransaction(c.Request.Context(), &Transaction{
+			FromAddress: tReq.FromAddress,
+			FromPubKey:  tReq.FromPubKey,
+			ToAddress:   tReq.ToAddress,
+			Amount:      tReq.Amount,
+		})
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
 			})
 		}
-		if clientUTXOs == nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "no UTXOs for this address",
-			})
-		}
-		var totalAmount int
-		for _, utxo := range clientUTXOs {
-			totalAmount += int(utxo.Output.Value)
-		}
-		if totalAmount < tReq.Amount {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": "not enough money",
-			})
-		}
-		prevBlock, err := h.service.BlockByHeight(c.Request.Context(), 0)
-		if err != nil {
-			log.Fatal(err)
-		}
-		prevBlockTx := prevBlock.GetTransactions()[len(prevBlock.GetTransactions())-1]
-		txInput := &proto.TxInput{
-			PrevTxHash: []byte(secure.HashTransaction(prevBlockTx)),
-			PublicKey:  tReq.FromPubKey,
-			OutIndex:   clientUTXOs[0].OutIndex,
-		}
-		txOutput1 := &proto.TxOutput{
-			Value:   int64(tReq.Amount),
-			Address: tReq.ToAddress,
-		}
-		txOutput2 := &proto.TxOutput{
-			Value:   int64(totalAmount - tReq.Amount),
-			Address: tReq.FromAddress,
-		}
-		tx = &proto.Transaction{
-			Inputs:  []*proto.TxInput{txInput},
-			Outputs: []*proto.TxOutput{txOutput1, txOutput2},
-		}
 	}
-	if tx != nil {
-		c.JSON(http.StatusOK, requests.InitTransactionResponse{
-			Transaction: tx,
-		})
-	}
+	c.JSON(http.StatusOK, requests.InitTransactionResponse{
+		Transaction: tx,
+	})
 }
 
 func (h *handler) NewTransaction(c *gin.Context) {
