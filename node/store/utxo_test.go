675ea2a4c4171f69c7f2150da89f0daf65c030de
Yuriy Kis <yurakis9@gmail.com>
2023-12-01 18:10:23 +0100

    feat: Add unit tests for UTXO store GetByAddress method
    This change adds unit tests for the GetByAddress method in the UTXO store. The tests verify that the method correctly retrieves UTXOs by address by creating a transaction, storing it in the memory transaction store, and then putting its UTXO in the memory UTXO store. The tests also ensure that the correct UTXOs are returned. By adding these tests, we ensure the accuracy and reliability of the GetByAddress method, improving overall system stability and correctness.
    
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- node/store/utxo_test.go
+++ node/store/utxo_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/yuriykis/microblocknet/common/crypto"
+	"github.com/yuriykis/microblocknet/common/proto"
 	"github.com/yuriykis/microblocknet/node/secure"
 	"github.com/yuriykis/microblocknet/node/util"
 )
@@ -37,5 +39,33 @@ func TestListUTXOs(t *testing.T) {
 }
 
 func TestGetUTXOByAddress(t *testing.T) {
-	// TODO: implement
+	txStore := NewMemoryTxStore()
+	privKey := crypto.GeneratePrivateKey()
+	tx := &proto.Transaction{
+		Inputs: []*proto.TxInput{},
+		Outputs: []*proto.TxOutput{
+			{
+				Value:   100000,
+				Address: privKey.PublicKey().Address().Bytes(),
+			},
+		},
+	}
+	err := txStore.Put(tx)
+	assert.Nil(t, err)
+
+	utxoStore := NewMemoryUTXOStore()
+	utxo := &proto.UTXO{
+		TxHash:   []byte(secure.HashTransaction(tx)),
+		OutIndex: 0,
+		Output:   tx.Outputs[0],
+		Spent:    false,
+	}
+	err = utxoStore.Put(utxo)
+	assert.Nil(t, err)
+
+	utxos, err := utxoStore.GetByAddress(privKey.PublicKey().Address().Bytes())
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(utxos))
+	assert.Equal(t, utxo, utxos[0])
+
 }
